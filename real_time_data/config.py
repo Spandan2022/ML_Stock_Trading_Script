36dcb702919233b2b9b939105f21c5b2f2d421da
Spandan Das <85077761+Spandan2022@users.noreply.github.com>
2024-01-31 12:22:07 -0800

    Update config.py

--- real_time_data/config.py
+++ real_time_data/config.py
@@ -2,7 +2,7 @@
 Overview: This file establishes constants required for establishing connection 
 with websockets to access real-time market data. 
 '''
-# Please note the API_KEY & SECRET_KEY are both dummy keys and therfore are not functional 
+# Please note the API_KEY & SECRET_KEY are both dummy keys and therefore are not functional 
 API_KEY = "PKA1C5GW4X4U2WGT809D" 
 SECRET_KEY = "0C0v9NZxGbHYafp4ZD0T925hj7H9bS0MTxqIsAud"
 ENDPOINT = "https://paper-api.alpaca.markets"
