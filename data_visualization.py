3022f4407a09302e58d5b8c6a95d0828f87141d8
Spandan2022 <2022das.spandan@gmail.com>
2024-05-09 17:00:18 -0700

    added ne folders

--- data_visualization.py
+++
@@ -1,7 +0,0 @@
-'''
-Overview: This file visualizes the real-time minute bars of a stock by taking the output 
-from the terminal and plotting it with matplotlibs.
-
-Resources: 
-    - https://youtu.be/CUFIjz_U7Mo (Subprocessing files)
-'''
--- machine_learning.py
+++
@@ -1,7 +0,0 @@
-import torch 
-
-x = torch.rand(3)
-print(x)
-
-y = torch.cuda.is_available()
-print(y)
