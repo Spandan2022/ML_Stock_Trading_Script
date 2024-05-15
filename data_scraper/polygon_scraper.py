b942f6a28053a56ff3d05abaa2839244456155b8
Kaleb Ugalde <kalebugalde@gmail.com>
2024-05-14 18:02:51 -0700

    Modified polygon_scraper.py

--- data_scraper/polygon_scraper.py
+++ data_scraper/polygon_scraper.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
+import time
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from polygon import RESTClient
 from polygon import WebSocketClient
@@ -11,20 +12,57 @@ from typing import List
 load_dotenv('secrets.env')
 API_KEY = os.environ["POLYGON_API_KEY"]
 
-client = RESTClient(api_key=API_KEY)
 
-ticker = "AAPL"
+def scrape_aggregate_data(client: RESTClient, ticker: str, start: datetime, end: datetime):
+    # If there's no folder with stock data -> create new folder with the ticker name
+    if not os.path.exists(ticker):
+        os.makedirs(ticker)
 
-start = "2023-01-03"
-end = "2023-01-03"
+    # Reference date for API call range
+    current_date = start
 
-aggs = []
-for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_=start, to=end, limit=50000):
-    aggs.append(a)
+    try:
+        # while current_date is sooner than the end date
+        while current_date >= end:
+            # Grab data every 90 days
+            past_date = current_date - timedelta(days=90)
 
-df = pd.DataFrame([agg.__dict__ for agg in aggs])
+            # Check if past_date is beyond the desired end date
+            if past_date < end:
+                past_date = end
 
-df["timestamp"] = df["timestamp"].apply(
-    lambda ts: datetime.fromtimestamp(ts/1000.0))
+            print(f"Gathering data from {current_date} to {past_date}")
 
-df.to_csv(f"AAPL-{start}-{end}.csv")
+            aggs = []
+            for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_=past_date, to=current_date, limit=50000):
+                aggs.append(a)
+
+            # Create and save data to csv file
+            df = pd.DataFrame([agg.__dict__ for agg in aggs])
+
+            df["timestamp"] = df["timestamp"].apply(
+                lambda ts: datetime.fromtimestamp(ts/1000.0))
+
+            start_string = current_date.strftime("%m-%d-%Y")
+            end_string = past_date.strftime("%m-%d-%Y")
+
+            df.to_csv(f"./{ticker}/{ticker}-{start_string}-{end_string}.csv")
+
+            # Set current_date to past_date minus 1 day
+            current_date = past_date - timedelta(days=1)
+
+            time.sleep(20)  # Rate limited to 5 API calls every minute
+    except:
+        return
+
+
+if __name__ == '__main__':
+    client = RESTClient(api_key=API_KEY)
+
+    ticker = "AAPL"
+
+    start = datetime(2024, 5, 1)  # Start of May cuz I felt like it
+    # Do over the span of two years not including the last month
+    end = start - timedelta(days=700)
+
+    scrape_aggregate_data(client, ticker, start, end)
