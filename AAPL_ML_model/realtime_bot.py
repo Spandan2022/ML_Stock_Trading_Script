7d2ff040c71e0bdff7ad7dc54379681c74f3c0f1
Spandan2022 <2022das.spandan@gmail.com>
2024-06-05 08:48:33 -0700

    updated shorts

--- AAPL_ML_model/realtime_bot.py
+++ AAPL_ML_model/realtime_bot.py
@@ -1,7 +1,17 @@
-# Using Alapaca API for real-time paper trading
+'''
+Using Alapaca API for real-time paper trading: 
+https://docs.alpaca.markets/docs/getting-started-with-trading-api
+
+WARNING:
+Note any order placed outside of NASDAQ trading hours will be queued 
+up for release the next trading day. 
+Extended hours trading can be accomplished however are higher risk 
+due to less liquidity. Alpaca currently supports these extended hours:
+    - Pre-market: 4:00am - 9:30am EST (Mon to Fri)
+    - After-hours: 4:00pm
+'''
 import config
 from alpaca.trading.client import TradingClient
-from alpaca.trading.requests import GetAssetsRequest
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.enums import OrderSide, TimeInForce
 
@@ -9,6 +19,10 @@ trading_client = TradingClient(config.API_KEY, config.SECRET_KEY, paper=True)
 
 # Get account info
 account = trading_client.get_account()
+
+# Declare symbol & quantity you want to short
+SYMBOL = 'NVDA'
+QNTY = 1
 
 # Check restrictions on account
 if account.trading_blocked:
@@ -18,32 +32,28 @@ if account.trading_blocked:
 print('${} is available for opening new posisitons.'.format(account.buying_power))
 
 # Check if particular symbol can be traded
-AAPL_asset = trading_client.get_asset('AAPL')
+asset = trading_client.get_asset(SYMBOL)
 
-if AAPL_asset.tradable:
-    print('AAPL is currently tradable')
+if asset.tradable:
+    print(SYMBOL + ' is currently tradable')
 
 
-# Placing orders on new positions 
-'''
-Note any order placed outside of NASDAQ trading hours will be queued 
-up for release the next trading day. 
-Extended hours trading can be accomplished however are higher risk 
-due to less liquidity. Alpaca currently supports these extended hours:
-    - Pre-market: 4:00am - 9:30am EST (Mon to Fri)
-    - After-hours: 4:00pm
-'''
-# https://docs.alpaca.markets/docs/working-with-orders 
-
-# Prepating market order
+# SHORTS:
+# Preparing market order
 market_order_data = MarketOrderRequest(
-                        symbol = "AAPL",
-                        qty = 1,
+                        symbol = SYMBOL,
+                        qty = QNTY,
                         side = OrderSide.SELL,
                         time_in_force = TimeInForce.GTC
                     )
-
 # Market order
 market_order = trading_client.submit_order(
                     order_data = market_order_data
                 )
+
+print(str(QNTY) + ' short of ' + SYMBOL + ' sucessfuly placed')
+
+portfolio = trading_client.get_all_positions()
+print('PORTFOLIO SUMMARY:')
+for position in portfolio:
+    print("{} shares of {}".format(position.qty, position.symbol))
--- real_time_data/config.py
+++ real_time_data/config.py
@@ -3,6 +3,6 @@ Overview: This file establishes constants required for establishing connection
 with websockets to access real-time market data. 
 '''
 # Please note the API_KEY & SECRET_KEY are both dummy keys and therefore are not functional 
-API_KEY = 'PK4CHC9HZWAYOKIZJUIM' 
-SECRET_KEY = '9KkoVNvrMqPeG1zR1xoWrd8tDcjOBpY23Y5uBD5J'
+API_KEY = 'PKIR7JNZZ97I940O5VF0' 
+SECRET_KEY = 'sTNZAfsKHf6FkyoHlftg5iOS5hARcohhcsUVB5gj'
 ENDPOINT = 'https://paper-api.alpaca.markets/v2'
